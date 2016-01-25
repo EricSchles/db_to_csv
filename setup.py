649d60ad7b95d2b5b12052cf832c0737249e3e2e
Eric Schles <ericschles@gmail.com>
2016-01-25 11:24:45 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name="db_api",
+    version="0.0",
+    description="a high level query engine to bring database tables locally for further inspection.",
+    author="Eric Schles",
+    packages=["db_api"]
+)
